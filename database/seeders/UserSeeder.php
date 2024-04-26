3da7d9ae64ca2d97bc2d7e6d99372adda21aa309
Ryan Miller <ryanmillergm@gmail.com>
2024-04-25 21:46:26 -0600

    Adds user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::factory([
+            'first_name'    => 'Ryan',
+            'last_name'     => 'Miller',
+            'email'         => 'ryanmillergm@gmail.com'
+        ])
+            ->create();
+
+        $user2 = User::factory([
+            'first_name'    => 'test',
+            'last_name'     => 'eleven',
+            'email'         => 'test11@test.com',
+        ])
+            ->create();
+    }
+}
