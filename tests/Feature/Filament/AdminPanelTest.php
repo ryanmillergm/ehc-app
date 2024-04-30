783310483d7c1509ffbbcd2bf34f861a81a8209f
Ryan Miller <ryanmillergm@gmail.com>
2024-04-30 17:31:01 -0600

    Adds authenticated user can visit admin panel test

--- tests/Feature/Filament/AdminPanelTest.php
+++ tests/Feature/Filament/AdminPanelTest.php
@@ -12,13 +12,24 @@ class AdminPanelTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_unauthenticated_users_cannot_visit_admin_page(): void
     {
-        $user = User::factory()->create();
-        $this->signIn($user);
-        
         $response = $this->get('/admin');
 
         $response->assertStatus(302);
+        $response->assertRedirect('/admin/login');
+    }
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_an_authenticated_user_can_visit_admin_page(): void
+    {
+        $user = User::factory()->create();
+        $this->signIn($user);
+
+        $response = $this->get('/admin');
+
+        $response->assertStatus(200);
     }
 }
