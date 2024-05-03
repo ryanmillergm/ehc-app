7ac3a604a0ed3d34885789c905b8e6cf93bc96ad
Ryan Miller <ryanmillergm@gmail.com>
2024-05-02 23:52:49 -0600

    Adds filter to not include attaching the owner to a team as an assigned team member

--- app/Filament/Resources/TeamResource/RelationManagers/UsersRelationManager.php
+++ app/Filament/Resources/TeamResource/RelationManagers/UsersRelationManager.php
@@ -3,12 +3,24 @@
 namespace App\Filament\Resources\TeamResource\RelationManagers;
 
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Resources\Pages\CreateRecord;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Actions\AttachAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\DetachAction;
+use Filament\Tables\Actions\DetachBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
 
 class UsersRelationManager extends RelationManager
 {
@@ -18,18 +30,24 @@ class UsersRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('first_name')
+                TextInput::make('first_name')
                     ->required()
                     ->maxLength(255),
-                Forms\Components\TextInput::make('last_name')
+                TextInput::make('last_name')
                     ->required()
                     ->maxLength(255),
-                Forms\Components\TextInput::make('email')
+                TextInput::make('email')
                     ->required()
                     ->maxLength(255),
-                Forms\Components\TextInput::make('current_team_id')
-                    ->required()
+                TextInput::make('current_team_id')
                     ->maxLength(255),
+                TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->maxLength(255)
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn ($livewire) => ($livewire instanceof CreateRecord))
             ]);
     }
 
@@ -40,24 +58,31 @@ class UsersRelationManager extends RelationManager
             ->columns([
                 Tables\Columns\TextColumn::make('details'),
             ])
+            ->inverseRelationship('assignedTeams')
             ->filters([
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
-                Tables\Actions\AttachAction::make()
+                CreateAction::make(),
+                AttachAction::make()
+                    ->recordSelectOptionsQuery(function (Builder $query, $livewire) {
+                        $query->whereDoesntHave('teams', function($query) use($livewire) {
+                            $query->where('user_id', $livewire->ownerRecord->user_id);
+                          });
+                    })
                     ->preloadRecordSelect()
                     ->multiple(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
-                Tables\Actions\DetachAction::make(),
+                EditAction::make(),
+                DeleteAction::make(),
+                DetachAction::make(),
+                ViewAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\DetachBulkAction::make(),
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    DetachBulkAction::make(),
                 ]),
             ]);
     }
