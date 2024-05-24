0e23dac26734aced22db08e409690ea2225aab7f
Ryan Miller <ryanmillergm@gmail.com>
2024-05-24 14:26:09 -0600

    Adds auth user must have permissions to access admin panel

--- tests/Feature/Filament/AdminPanelTest.php
+++ tests/Feature/Filament/AdminPanelTest.php
@@ -12,7 +12,7 @@ class AdminPanelTest extends TestCase
     use WithFaker, RefreshDatabase;
 
     /**
-     * A basic feature test example.
+     * Unauthenticated users cannot visit admin page.
      */
     public function test_unauthenticated_users_cannot_visit_admin_page(): void
     {
@@ -23,12 +23,25 @@ class AdminPanelTest extends TestCase
     }
 
     /**
-     * A basic feature test example.
+     * An authenticated user that doesn't have permissions cannot visit admin page.
      */
-    public function test_an_authenticated_user_can_visit_admin_page(): void
+    public function test_an_authenticated_user_without_permissions_cannot_can_visit_admin_page(): void
     {
         $user = User::factory()->create();
         $this->signIn($user);
+
+        $response = $this->get('/admin');
+
+        $response->assertStatus(403);
+    }
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_an_authenticated_user_with_permissions_can_visit_admin_page(): void
+    {
+        $user = User::factory()->create();
+        $this->signInAsSuperAdmin($user);
 
         $response = $this->get('/admin');
 
