e163041b43d7ec7622deba994c326afe4adde69a
Ryan Miller <ryanmillergm@gmail.com>
2024-05-24 14:26:37 -0600

    Adds signInAsSuperAdmin function for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ namespace Tests;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -12,6 +14,16 @@ abstract class TestCase extends BaseTestCase
         $user = $user ?: User::factory()->create();
 
         $this->actingAs($user);
+
+        return $user;
+    }
+
+    protected function signInAsSuperAdmin($user = null)
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        $role = Role::create(['name' => 'Super Admin']);
+        $user = $this->signIn($user);
+        $user->assignRole($role);
 
         return $user;
     }
