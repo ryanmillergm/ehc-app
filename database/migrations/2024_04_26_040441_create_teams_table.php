0fef39a2c44b1c7a6e2c0dd7ba3e53ccefbb5e54
Ryan Miller <ryanmillergm@gmail.com>
2024-09-13 12:58:46 -0600

    Adds slug to teams for multi tenancy url

--- database/migrations/2024_04_26_040441_create_teams_table.php
+++ database/migrations/2024_04_26_040441_create_teams_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id');
             $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
