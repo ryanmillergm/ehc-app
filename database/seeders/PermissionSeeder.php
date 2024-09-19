8de7cf45eaab65f330dea3eaec058a374c2ebd44
Ryan Miller <ryanmillergm@gmail.com>
2024-09-19 12:30:42 -0600

    Updates permissions seeder with new permissions

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -18,37 +18,27 @@ class PermissionSeeder extends Seeder
         // Reset cached roles and permissions
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // Roles & assign permissions to roles
-        // Role::create(['name' => 'Admin'])->givePermissionTo(
-        //     Permission::all()
-        // );
+        Permission::create(['name' => 'admin.panel']);
+        Permission::create(['name' => 'org.panel']);
 
-        $role = Role::create(['name' => 'Super Admin']);
-        $role = Role::create(['name' => 'Admin']);
+        Permission::create(['name' => 'users.read']);
+        Permission::create(['name' => 'users.write']);
+        Permission::create(['name' => 'users.delete']);
 
-        //Some initial role configuration
-        // $roles = [
-        //     'Admin' => [
-        //         'view posts',
-        //         'create posts',
-        //         'update posts',
-        //         'delete posts',
-        //     ],
-        //     'Editor' => [
-        //         'view posts',
-        //         'create posts',
-        //         'update posts'
-        //     ],
-        //     'Member' => [
-        //         'view posts'
-        //     ]
-        // ];
+        Permission::create(['name' => 'permissions.read']);
+        Permission::create(['name' => 'permissions.write']);
+        Permission::create(['name' => 'permissions.delete']);
 
-        // collect($roles)->each(function ($permissions, $role) {
-        //     $role = Role::findOrCreate($role);
-        //     collect($permissions)->each(function ($permission) use ($role) {
-        //         $role->permissions()->save(Permission::findOrCreate($permission));
-        //     });
-        // });
+        Permission::create(['name' => 'roles.read']);
+        Permission::create(['name' => 'roles.write']);
+        Permission::create(['name' => 'roles.delete']);
+
+        Permission::create(['name' => 'teams.read']);
+        Permission::create(['name' => 'teams.write']);
+        Permission::create(['name' => 'teams.delete']);
+
+        Permission::create(['name' => 'children.read']);
+        Permission::create(['name' => 'children.write']);
+        Permission::create(['name' => 'children.delete']);
     }
 }
