d4a7529fa6331718e0f49948bbcbad91e0fd8dd0
Ryan Miller <ryanmillergm@gmail.com>
2024-09-20 16:01:54 -0600

    Updates permissions test and adds relation to roles test

--- tests/Feature/Spatie/PermissionsTest.php
+++ tests/Feature/Spatie/PermissionsTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Spatie;
 
+use App\Models\Role;
+use ErrorException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Spatie\Permission\Models\Permission;
@@ -16,9 +18,33 @@ class PermissionsTest extends TestCase
      */
     public function test_permission_requires_a_name(): void
     {
-        $permission = Permission::create(['name' => '', 'guard_name' => 'web']);
-        // $permission = Permission::create(['name' => 'edit articles']);
+        $this->expectException(ErrorException::class);
+        $this->expectExceptionMessage('Undefined array key "name"');
 
-        $this->assertDatabaseCount('permissions', 0);
+        $permission = Permission::create(['guard_name' => 'web']);
+    }
+
+    public function test_a_permission_can_belong_to_a_role(): void
+    {
+        $permission = Permission::create(['name' => 'admin.panel']);
+
+        $role = Role::create(['name' => 'Super Admin'])->givePermissionTo($permission);
+
+        $this->assertTrue($role->hasPermissionTo('admin.panel'));
+    }
+
+    public function test_a_permission_can_belong_to_many_roles(): void
+    {
+        $permission = Permission::create(['name' => 'admin.panel']);
+
+        $superAdmin = Role::create(['name' => 'Super Admin'])->givePermissionTo($permission);
+        $admin = Role::create(['name' => 'Admin'])->givePermissionTo($permission);
+        $editor = Role::create(['name' => 'Editor'])->givePermissionTo($permission);
+
+        $this->assertTrue($superAdmin->hasPermissionTo('admin.panel'));
+        $this->assertTrue($admin->hasPermissionTo('admin.panel'));
+        $this->assertTrue($editor->hasPermissionTo('admin.panel'));
+
+        $this->assertCount(3, $permission->roles);
     }
 }
