c4885356f8f494b56d4b292b0531590d2084c3b1
Ryan Miller <ryanmillergm@gmail.com>
2024-09-24 14:09:36 -0600

    Adds shared success message

---
+++ resources/views/shared/success-message.blade.php
@@ -0,0 +1,16 @@
+@if (session()->has('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{session('success')}}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+
+
+    <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+        <div class="flex">
+          <div>
+            <p class="font-bold"></p>
+            <p class="text-sm">{{session('success')}}</p>
+          </div>
+        </div>
+      </div>
+@endif
