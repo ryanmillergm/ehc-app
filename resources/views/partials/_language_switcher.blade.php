a87811daf652de7bde32643f3aab0ea17c5fac35
Ryan Miller <ryanmillergm@gmail.com>
2024-09-24 14:08:42 -0600

    Adds language switcher partial

---
+++ resources/views/partials/_language_switcher.blade.php
@@ -0,0 +1,4 @@
+<div class="text-center py-2">
+    <a class="btn btn-link btn-sm" href="{{ route('lang', ['lang' => 'en']) }}">en</a>
+    <a class="btn btn-link btn-sm" href="{{ route('lang', ['lang' => 'es']) }}">es</a>
+</div>
