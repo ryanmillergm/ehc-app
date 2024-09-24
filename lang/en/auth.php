14738e64ef2320e28bfc29118b833905f9b76ebf
Ryan Miller <ryanmillergm@gmail.com>
2024-09-24 13:37:47 -0600

    Adds languages with en and es

---
+++ lang/en/auth.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'                => 'These credentials do not match our records.',
+    'password'              => 'The provided password is incorrect.',
+    'throttle'              => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    // User Input
+    'first_name'            => 'First Name',
+    'last_name'             => 'Last Name',
+    'email'                 => 'Email',
+    'password'              => 'Password',
+    'password_confirmation' => 'Confirm Password',
+
+    // User Register Input
+    'already_registered'    => 'Already registered?',
+    'register'              => 'Register',
+
+    // User Login Input
+    'forgot_your_password'  => 'Forgot your password?',
+    'remember_me'           => 'Remember me',
+    'log_in'                => 'Log in',
+    'log_out'               => 'Log Out',
+    'log_out_other_browser_sessions' => 'Log Out Other Browser Sessions',
+
+    // Forgot Password
+    'email_password_reset_link' => 'Email Password Reset Link',
+
+    // Confirm
+    'confirm'               => 'Confirm',
+    'confirm_password'      => 'Confirm Password',
+    'reset_password'        => 'Reset Password',
+];
