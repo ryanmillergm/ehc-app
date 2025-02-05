f087ac3d55171143677bef484430350840088cdb
Ryan Miller <ryanmillergm@gmail.com>
2025-02-04 19:10:38 -0700

    Fixes syntax for permission required to "pages"

--- app/Http/Requests/Pages/StorePageRequest.php
+++ app/Http/Requests/Pages/StorePageRequest.php
@@ -11,7 +11,7 @@ class StorePageRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return $this->user()->hasPermissionTo('pages.create');
     }
 
     /**
