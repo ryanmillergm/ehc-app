6e38f7a9369b52f1cc006f503581a07d9b6987f6
Ryan Miller <ryanmillergm@gmail.com>
2025-02-04 18:32:38 -0700

    Adds tests a page can be created by a super admin

--- tests/Feature/Pages/PageTest.php
+++ tests/Feature/Pages/PageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Pages;
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -20,5 +21,24 @@ class PageTest extends TestCase
           Schema::hasColumns('pages', [
             'id', 'title', 'is_active',
         ]), 1);
+    }
+
+    /**
+     * A Page can be created test
+     */
+    public function test_a_page_can_be_created_by_super_admin(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $user = $this->signInAsSuperAdmin();
+
+        $attributes = [
+            'title'         => 'Blog Test Example',
+            'is_active'     => true,
+        ];
+
+        $response = $this->post('/pages', $attributes);
+
+        $this->assertDatabaseHas('pages', $attributes);
     }
 }
