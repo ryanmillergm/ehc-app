c56bcaaa0cc034bd1840749878a9d7dac3476500
Ryan Miller <ryanmillergm@gmail.com>
2025-02-06 19:25:12 -0700

    Adds notifcations for saving or updating pages and redirect to index after

--- app/Filament/Resources/PageResource/Pages/CreatePage.php
+++ app/Filament/Resources/PageResource/Pages/CreatePage.php
@@ -9,4 +9,14 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePage extends CreateRecord
 {
     protected static string $resource = PageResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Page created';
+    }
 }
--- app/Filament/Resources/PageResource/Pages/EditPage.php
+++ app/Filament/Resources/PageResource/Pages/EditPage.php
@@ -17,4 +17,14 @@ class EditPage extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotificationTitle(): ?string
+    {
+        return 'Page updated';
+    }
 }
