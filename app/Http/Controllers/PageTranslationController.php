ac9559118ce618179536d0957fcbc88e40b4293c
Ryan Miller <ryanmillergm@gmail.com>
2025-02-10 16:52:14 -0700

    Adds page translations migration, model, controller, policy, factory and seederr

---
+++ app/Http/Controllers/PageTranslationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PageTranslation;
+use App\Http\Requests\StorePageTranslationRequest;
+use App\Http\Requests\UpdatePageTranslationRequest;
+
+class PageTranslationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePageTranslationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PageTranslation $pageTranslation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PageTranslation $pageTranslation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePageTranslationRequest $request, PageTranslation $pageTranslation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PageTranslation $pageTranslation)
+    {
+        //
+    }
+}
