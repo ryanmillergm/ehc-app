62afada357a72900993fe7bf9cf5bb0f9b25d2d5
Ryan Miller <ryanmillergm@gmail.com>
2025-02-11 09:29:22 -0700

    Adds store and update page translation request validations

---
+++ app/Http/Requests/Pages/StorePageTranslationRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Pages;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePageTranslationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->hasPermissionTo('pages.create');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'page_id'       => 'required',
+            'language_id'   => 'required',
+            'title'         => 'required',
+            'slug'          => 'required',
+            'description'   => 'required',
+            'content'       => 'required',
+            'is_active'     => 'required',
+        ];
+    }
+}
