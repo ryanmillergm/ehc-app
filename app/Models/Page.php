4ae62ca905c4993e28e82136c438079898a99caf
Ryan Miller <ryanmillergm@gmail.com>
2025-02-11 09:29:56 -0700

    Adds a page has many translations relationship

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Page extends Model
 {
@@ -18,4 +20,26 @@ class Page extends Model
         'title',
         'is_active',
     ];
+
+    /**
+     * Get the translations for a page.
+     */
+    public function translations(): HasMany
+    {
+        return $this->hasMany(PageTranslation::class);
+    }
+
+    /**
+     * Get the Page that owns the PageTranslation.
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Page::class);
+    }
+
+    public function addTranslation($request)
+    {
+        return $this->translations()->create($request);
+    }
+
 }
