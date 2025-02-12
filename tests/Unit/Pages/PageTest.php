34c5e4fc72d0b7def02612b0f22dc4b234ea24be
Ryan Miller <ryanmillergm@gmail.com>
2025-02-12 11:34:19 -0700

    Adds a page has many translations relationship test

--- tests/Unit/Pages/PageTest.php
+++ tests/Unit/Pages/PageTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Pages;
 
 use App\Models\Language;
 use App\Models\Page;
+use App\Models\PageTranslation;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -33,6 +34,27 @@ class PageTest extends TestCase
 
         $page->addTranslation($attributes);
 
-        $this->assertCount(1, $page->translations);
+        $this->assertCount(1, $page->pageTranslations);
+    }
+
+    /**
+     * Test a Page has many PageTranslations relationship
+     */
+    public function test_a_page_has_many_page_translations()
+    {
+        $page = Page::factory()->create();
+
+        $language = Language::factory()->create();
+
+        $translation = PageTranslation::factory()->create(['language_id' => $language->id, 'page_id' => $page->id]);
+
+        // Method 1: A Page Translation exists in a Languages pageTranslation collections.
+        $this->assertTrue($page->pageTranslations->contains($translation));
+
+        // Method 2: Count that a post comments collection exists.
+        $this->assertEquals(1, $page->pageTranslations->count());
+
+        // Method 3: PageTranslations are related to Pages and is a collection instance.
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $page->pageTranslations);
     }
 }
