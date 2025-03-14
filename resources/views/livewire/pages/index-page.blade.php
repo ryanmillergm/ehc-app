7f534ac911f07be7518e1096b4a78ae65fe8998c
Ryan Miller <ryanmillergm@gmail.com>
2025-03-14 15:03:39 -0600

    Updates index livewire view to show page list with link and description

--- resources/views/livewire/pages/index-page.blade.php
+++ resources/views/livewire/pages/index-page.blade.php
@@ -1,8 +1,11 @@
 <div>
     <h1>Hello From Pages</h1>
+    
             
         @foreach ($this->translations as $translation)
-            {{ $translation->title }}
+            <h1>{{ $translation->title }}</h1>
+            <h1><a href="{{ url( "/pages/{$translation->slug}" ) }}" class="font-medium">{{ $translation->title }}</a></h1>
+            <p>{{ $translation->description }}</p>
         @endforeach
         
 </div>
