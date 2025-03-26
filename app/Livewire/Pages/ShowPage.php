5ef93598c7739543458fd9ccd59994e8b70f9e89
Ryan Miller <ryanmillergm@gmail.com>
2025-03-26 09:47:53 -0600

    Fixes redirect forever issue when a user is redirect to a different translation

--- app/Livewire/Pages/ShowPage.php
+++ app/Livewire/Pages/ShowPage.php
@@ -4,11 +4,15 @@ namespace App\Livewire\Pages;
 
 use App\Models\Language;
 use App\Models\PageTranslation;
+use Livewire\Attributes\Session;
 use Livewire\Component;
 
 class ShowPage extends Component
 {
     public $translation;
+
+    #[Session(key: 'redirect_from_translation')]
+    public $redirectFromTranslation = false;  
 
     public function mount($slug) {
         $this->translation = $this->findTranslationBySlugOrDefault($slug);
@@ -20,9 +24,16 @@ class ShowPage extends Component
         $translation = $translationQuery->get()->first();
         
         if (!$translation || !$translation->page->is_active) {
+            session()->flash('status', "Sorry. The page you were looking for does not exist.");
             return $this->redirectToPages($slug);
         }
         
+        if ($this->redirectFromTranslation) {
+            $this->toggleRedirectFromTranslation();
+            
+            return $translation;
+        }
+
         $language = Language::find(session('language_id')) ?? getLanguage();
         
         // Translation By Slug Doesn't Match Current Language
@@ -31,20 +42,23 @@ class ShowPage extends Component
             $translation_for_current_language = $page->pageTranslations->where('language_id', $language->id)->first();
             // If translation found for current Language redirect to translations slug
             if ($translation_for_current_language) {
-                // session()->flash('status', `Found Page for $language->title language.`);
+                session()->flash('status', "Found Page for {$language->title} language.");
                 // $this->translation = $translation_for_current_language;
+
+                $this->toggleRedirectFromTranslation();
                 return $this->redirect('/pages/' . $translation_for_current_language->slug, navigate: true);  
             } 
             
             $default_language = Language::first();
-            // session()->flash('status', `$language->title page not available.`);
+            session()->flash('status', "{$language->title} page not available.");
             
             $translation_for_default_language = $page->pageTranslations->where('language_id', $default_language->id)->first();
             
             // If translation for default language redirect to translations slug
             if ($translation_for_default_language) {
-                // session()->flash('status', `Found $default_language->title page intead.`);
+                session()->flash('status', "Found {$default_language->title} page intead.");
                 // $this->translation = $translation_for_default_language;
+                $this->toggleRedirectFromTranslation();
                 return $this->redirect('/pages/' . $translation_for_default_language->slug, navigate: true);
             }
 
@@ -52,14 +66,20 @@ class ShowPage extends Component
             if($translation) {
                 $translation_language = Language::find($translation->language_id);
 
-                // session()->flash('status', `Found $translation_language->title page intead.`);
+                session()->flash('status', "Found {$translation_language->title} page intead.");
                 $this->translation = $translation;
             } else {
+                session()->flash('status', "Sorry. The page you were looking for does not exist");
                 return $this->redirectToPages($slug);
             }
         }
 
         return $translation;
+    }
+
+    public function toggleRedirectFromTranslation()
+    {
+        $this->redirectFromTranslation = !$this->redirectFromTranslation;
     }
 
     public function redirectToPages($slug) 
