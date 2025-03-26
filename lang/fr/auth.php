5b1ee05b9b37da483de008c23a38aa9b1aeb2575
Ryan Miller <ryanmillergm@gmail.com>
2025-03-26 09:46:16 -0600

    Adds french translations

---
+++ lang/fr/auth.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'                => 'These credentials do not match our records.',
+    'password'              => 'The provided password is incorrect.',
+    'throttle'              => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    // User Input
+    'first_name'            => 'Prénom',
+    'last_name'             => 'Nom de famille',
+    'email'                 => 'Email',
+    'password'              => 'Password',
+    'password_confirmation' => 'Confirm Password',
+
+    // User Register Input
+    'already_registered'    => 'Already registered?',
+    'register'              => 'Registre',
+
+    // User Login Input
+    'forgot_your_password'  => 'Forgot your password?',
+    'remember_me'           => 'Remember me',
+    'log_in'                => 'Se connecter',
+    'log_out'               => 'Se déconnecter',
+    'log_out_other_browser_sessions' => 'Log Out Other Browser Sessions',
+
+    // Forgot Password
+    'email_password_reset_link' => 'Email Password Reset Link',
+
+    // Confirm
+    'confirm'               => 'Confirm',
+    'confirm_password'      => 'Confirm Password',
+    'reset_password'        => 'Reset Password',
+];
---
+++ lang/fr/flash-messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'success'           => 'Succès!',
+
+    'language_updated'  => 'La langue a été mise à jour en français.',
+
+];
