0ec7fe42e957d376b30f2b2ab4c608af48352672
Ryan Miller <ryanmillergm@gmail.com>
2025-03-25 22:11:31 -0600

    Adds show session status if exists for show page

--- resources/views/livewire/pages/show-page.blade.php
+++ resources/views/livewire/pages/show-page.blade.php
@@ -1,4 +1,10 @@
 <div>
+    @if (session('status'))
+        <div class="text-center bg-green-700 text-gray-200">
+            {{ session('status') }}
+        </div>
+    @endif
+
     <h1>This is the show page</h1>
     
     <h1>{{ $this->translation->title }}</h1>
