06c7ffeb40241891a406dec6d1d7b05809bf2ef4
Ryan Miller <ryanmillergm@gmail.com>
2025-11-15 16:20:11 -0700

    Adds comments for clarity while developing.

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,3 +1,5 @@
+{{-- Jetstream Welcome Page --}}
+<h1>resources\views\components\welcome.blade.php</h1>
 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
     <x-application-logo class="block h-12 w-auto" />
 
