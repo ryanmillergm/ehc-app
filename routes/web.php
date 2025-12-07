0241aa01a1a817abea8d6c423866864c0999f69f
Ryan Miller <ryanmillergm@gmail.com>
2025-12-06 19:50:50 -0700

    Adds dashboard controller route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\TeamController;
 use App\Http\Controllers\GivingController;
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\ChildrenController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LanguagesController;
 use App\Http\Controllers\StripeWebhookController;
 use App\Http\Controllers\PageTranslationController;
@@ -68,9 +69,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
     // Addresses
     Route::get('/addresses', [AddressController::class, 'index'])
