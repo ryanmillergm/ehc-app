5e83149d98ec46aafd47b71df10295854974fac6
Ryan Miller <ryanmillergm@gmail.com>
2026-01-02 11:12:19 -0700

    Adds "@tailwindcss/vite" for custom filament theme styling

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -7,8 +8,10 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/filament/admin/theme.css',
             ],
             refresh: true,
         }),
+        tailwindcss(),
     ],
 });
